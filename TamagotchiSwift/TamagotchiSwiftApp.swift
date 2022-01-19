bba57d7d9ff38cd390403e679e9b3f8839642221
Ethan <63197802+SpicyChair@users.noreply.github.com>
2022-01-19 09:55:37 +0000

    Initial Commit

---
+++ TamagotchiSwift/TamagotchiSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiSwiftApp.swift
+//  TamagotchiSwift
+//
+//  Created by Hin, Ethan-Scott (WING) on 19/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
